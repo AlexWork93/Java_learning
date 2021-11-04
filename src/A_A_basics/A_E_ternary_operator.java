2ec971179e9b07dacd984eaa155450a2326169ee
AlexWork93 <ortwinkrausemail@gmail.com>
2021-11-04 22:52:57 +0200

    my commit

---
+++ src/A_A_basics/A_E_ternary_operator.java
@@ -0,0 +1,12 @@
+package A_A_basics;
+
+public class A_E_ternary_operator {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(num == 5 ? "this is five" : "this is not five");
+        num += 1;
+        System.out.println(num == 5 ? "this is five" : "this is not five");
+
+        //      CONDITION ? BLOCK IF CONDITION TRUE : BLOCK IF CONDITION FALSE
+    }
+}
